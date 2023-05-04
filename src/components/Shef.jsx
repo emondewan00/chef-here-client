a93e902bbcc8740b69d4765b235c5e37f7f9639d
emondewan00 <dewanmohammademon@gmail.com>
2023-05-04 06:32:06 +0600

    edit card

--- src/components/Shef.jsx
+++ src/components/Shef.jsx
@@ -5,14 +5,14 @@ const Shef = () => {
     <div className="mt-10 px-3 md:px-0 mx-auto container">
       <h1 className="text-4xl  text-center font-semibold "> Get Best One</h1>
       <div className="my-8 grid grid-cols-4 justify-items-center ">
-        <div className="card border card-compact w-80 bg-gray-50 shadow-xl">
+        <div className="  card-compact w-80 bg-gray-50 ">
           <figure>
             <img src="/shef1 (1).jpg" alt="" className="p-4 rounded-xl" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Name:Jhon Deo</h2>
-            <p className="text-lg">
-              <span>Experience:5 years</span> <span>Recipes:5</span>
+            <h2 className="card-title">Jhon Deo</h2>
+            <p className="text-xl text-black">
+              <span >5 years experiance</span> <span>Recipes:5</span>
             </p>
 
             <div className="card-actions justify-between border-t pt-2 items-center text-lg">
@@ -24,14 +24,14 @@ const Shef = () => {
             </div>
           </div>
         </div>
-        <div className="card border card-compact w-80 bg-gray-50 shadow-xl">
+        <div className=" card-compact w-80 bg-gray-50 ">
           <figure>
             <img src="/shef1 (1).jpg" alt="" className="p-4 rounded-xl" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Name:Jhon Deo</h2>
-            <p className="text-lg">
-              <span>Experience:5 years</span> <span>Recipes:5</span>
+            <h2 className="card-title">Jhon Deo</h2>
+            <p className="text-xl text-black">
+              <span >5 years experiance</span> <span>Recipes:5</span>
             </p>
 
             <div className="card-actions justify-between border-t pt-2 items-center text-lg">
@@ -43,14 +43,14 @@ const Shef = () => {
             </div>
           </div>
         </div>
-        <div className="card border card-compact w-80 bg-gray-50 shadow-xl">
+        <div className=" card-compact w-80 bg-gray-50 ">
           <figure>
             <img src="/shef1 (1).jpg" alt="" className="p-4 rounded-xl" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Name:Jhon Deo</h2>
-            <p className="text-lg">
-              <span>Experience:5 years</span> <span>Recipes:5</span>
+            <h2 className="card-title">Jhon Deo</h2>
+            <p className="text-xl text-black">
+              <span >5 years experiance</span> <span>Recipes:5</span>
             </p>
 
             <div className="card-actions border-t pt-2 items-center justify-between">
@@ -62,14 +62,14 @@ const Shef = () => {
             </div>
           </div>
         </div>
-        <div className="card border card-compact w-80 bg-gray-50 shadow-xl">
+        <div className=" card-compact w-80 bg-gray-50 ">
           <figure>
             <img src="/shef1 (1).jpg" alt="" className="p-4 rounded-xl" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Name:Jhon Deo</h2>
-            <p className="text-lg">
-              <span>Experience:5 years</span> <span>Recipes:5</span>
+            <h2 className="card-title">Jhon Deo</h2>
+            <p className="text-xl text-black">
+              <span >5 years experiance</span> <span>Recipes:5</span>
             </p>
 
             <div className="card-actions justify-between border-t pt-2 items-center text-lg">
